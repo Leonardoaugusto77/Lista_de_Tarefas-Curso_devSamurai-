544d2beeb909bab5c7471a1a289cd70221aa78db
Heisen <leonardopjh30@gmail.com>
2023-06-13 14:46:39 -0300

    feat(class): created the functions responsible for capturing the task

--- src/App.js
+++ src/App.js
@@ -1,3 +1,42 @@
+import { useState } from "react";
+import index from "./index.css";
+
 export default function App() {
-  return <></>;
+  const [value, setValue] = useState("");
+
+  const targetValue = (e) => {
+    setValue(e.target.value);
+  };
+
+  const erase = () => {
+    setValue("");
+  };
+
+  const submitValue = (e) => {
+    const ESCAPE_KEY = 27;
+    const ENTER_KEY = 13;
+
+    if (e.which === ENTER_KEY) {
+      console.log(value);
+      erase();
+    } else if (e.which === ESCAPE_KEY) {
+      erase();
+    }
+  };
+
+  return (
+    <section id="app" className="container">
+      <h1 className="title">Lista de tarefas</h1>
+      <div className="main">
+        <input
+          className="newTodo"
+          placeholder="O que precisa ser feito!"
+          type="text"
+          value={value}
+          onChange={targetValue}
+          onKeyDown={submitValue}
+        />
+      </div>
+    </section>
+  );
 }
